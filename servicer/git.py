f60d9e39a32e47c0798e5b71415cf7f17df5bc83
Matt Groot <mgroot@sparkcognition.com>
2018-07-23 22:49:49 -0500

    [git] wrap https repository in quotes

--- servicer/git.py
+++ servicer/git.py
@@ -82,7 +82,7 @@ class Git():
         if protocol == 'ssh':
             self.run('git push %s %s' % (origin, branch))
         elif protocol == 'https':
-            self.run('git push https://%s:%s@%s %s:%s' % (os.environ['GIT_USERNAME'], os.environ['GIT_PASSWORD'], os.environ['GIT_REPOSITORY'], branch, branch))
+            self.run('git push "https://%s:%s@%s" %s:%s' % (os.environ['GIT_USERNAME'], os.environ['GIT_PASSWORD'], os.environ['GIT_REPOSITORY'], branch, branch))
         else:
             raise ValueError('Invalid git push protocol: %s' % protocol)
 
