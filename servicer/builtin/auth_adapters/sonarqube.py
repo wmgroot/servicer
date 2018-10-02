6d9a36b016c54d44c19e07033f52986a756a74ea
Matt Groot <mgroot@sparkcognition.com>
2018-10-02 16:23:40 -0500

    [sonarqube-auth-adapter] download os agnostic scanner

--- servicer/builtin/auth_adapters/sonarqube.py
+++ servicer/builtin/auth_adapters/sonarqube.py
@@ -12,11 +12,10 @@ class AuthAdapter(BaseAuthAdapter):
         self.sonarqube_token = os.getenv('SONARQUBE_TOKEN', self.config.get('auth_token'))
 
         self.scanner_version = self.config['scanner_version']
-        self.scanner_os = self.config.get('scanner_os', 'linux')
         self.scanner_path = self.config.get('scanner_path', './')
 
         if self.scanner_path.endswith('/'):
-            self.scanner_path = '%ssonar-scanner-cli-%s-%s' % (self.scanner_path, self.scanner_version, self.scanner_os)
+            self.scanner_path = '%ssonar-scanner-cli-%s' % (self.scanner_path, self.scanner_version)
 
     def authenticate(self):
         if self.sonarqube_token:
@@ -28,7 +27,7 @@ class AuthAdapter(BaseAuthAdapter):
         if os.path.exists(self.scanner_path):
             print('found sonar-scanner installation at %s' % self.scanner_path)
         else:
-            download_file = 'sonar-scanner-cli-%s-%s.zip' % (self.scanner_version, self.scanner_os)
+            download_file = 'sonar-scanner-cli-%s.zip' % self.scanner_version
             download_directory = os.path.dirname(self.scanner_path)
             download_path = '%s/%s' % (download_directory, download_file)
             url = 'https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/%s' % download_file
@@ -37,10 +36,8 @@ class AuthAdapter(BaseAuthAdapter):
             print('downloading sonar-scanner from %s to %s' % (url, download_path))
             urllib.request.urlretrieve(url, download_path)
 
-            # if os.path.exists(download_path):
-            #     self.run('rm -r %s' % download_path)
             self.run('unzip -d `dirname %s` %s' % (download_path, download_path))
             self.run('rm %s' % download_path)
 
-            extracted_directory = 'sonar-scanner-%s-%s' % (self.scanner_version, self.scanner_os)
+            extracted_directory = 'sonar-scanner-%s' % self.scanner_version
             self.run('mv %s/%s %s' % (download_directory, extracted_directory, self.scanner_path))
