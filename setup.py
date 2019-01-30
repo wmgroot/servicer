730c2d4e1c5def0596417af63b29d5eee3580cab
Matt Groot <wmgroot@gmail.com>
2019-01-29 23:44:31 -0600

    add pyOpenSSL as a dependency

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     },
     include_package_data = True,
     install_requires = [
+        'pyOpenSSL==19.0.0',
         'PyYAML==4.2b4',
         'requests==2.20.1',
     ],
@@ -30,5 +31,5 @@ setup(
     packages = find_packages(),
     python_requires = '>=3.5',
     url = 'https://github.com/wmgroot/servicer',
-    version = '0.10.2',
+    version = '0.10.3',
 )
